a25093b645544c369481408367346aeb202cd7a2
Nikoloz Naskidashvili <NikolozNaskidashvili@gmail.com>
2026-09-22 17:18:51 +0400

    first commit

---
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("{cityNode}")]
+        public IActionResult GetWeather(string cityNode)
+        {
+            return Ok( new
+            {
+                temperatureC = 25,
+                Description = "Sunny",
+                city = cityNode
+            });
+        }
+    }
+}
---
+++ WeatherAPI/WeatherApiSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace WeatherAPI
+{
+    public class WeatherApiSettings
+    {
+    }
+}
